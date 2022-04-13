07c043b253f0022d501773dcca0131efacfe53e3
Nwokolo_chibuzo <nwokolo_chibuzo@outlook.com>
2022-04-13 15:21:00 +0200

    added dev start script

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,76 +1,66 @@
 const User = require("../core/models/user");
-const {body, validationResult } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const user = require("../core/models/user");
 const secretkey = "abcdefgh12359800";
 
-function createNewUser(req) {
+async function createNewUser(req) {
+  let createdUser;
   try {
-    let user;
-    bcrypt
-      .hash(req.password, 12)
-      .then((hashedPw) => {
-        user = new User({
-          email: req.email,
-          password: hashedPw,
-        });
-         user.save();
-      })
-      return user;
-  } catch (error) {
-    throw error;
+    hashedPw = await bcrypt.hash(req.password, 12);
+    let user = new User({
+      email: req.email,
+      password: hashedPw,
+    });
+
+    createdUser = await user.save();
+  } catch (err) {
+    throw err;
   }
+
+  return createdUser;
 }
 
-exports.login = (req, res, next) => {
-    console.log(req.body.email);
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error('Validation failed.');
-    error.statusCode = 422;
-    error.data = errors.array();
-    throw error;
-  }
-  const email = req.body.email;
-  const password =  req.body.password;
-  let loadedUser;
-  User.findOne({ email: email })
-    .then((user) => {
+exports.login = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new Error("Validation failed.");
+      error.statusCode = 422;
+      error.data = errors.array();
+      throw error;
+    }
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadedUser;
+    let user = await User.findOne({ email: email });
 
-      if (!user) {
-        let  newUser = new User({
-          email: email,
-          password: password,
-        });
-        user = createNewUser(newUser); 
-      } 
-      if(user === undefined)
-      {
-          return res.status(400).json({"message":"could not create user"});
-      }
-      loadedUser = user;
-      return bcrypt.compare(password, user.password);
-      
-    })
-    .then((isEqual) => {
-      if (!isEqual) {
-        const error = new Error("Wrong password");
-        error.statusCode = 401;
-        throw error;
-      }
-
-      const token = jwt.sign({ email: loadedUser.email }, secretkey, {
-        expiresIn: "1h",
+    if (!user) {
+      let newUser = new User({
+        email: email,
+        password: password,
       });
-      console.log(token);
+      user = await createNewUser(newUser);
+      console.log(user);
+    }
+    if (!user) {
+      return res.status(400).json({ message: "could not create user" });
+    }
+    loadedUser = user;
+    isEqual = await bcrypt.compare(password, user.password);
 
-      res.status(200).json({ token: token, Email: loadedUser.email });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+    if (!isEqual) {
+      const error = new Error("Wrong password");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const token = jwt.sign({ email: loadedUser.email }, secretkey, {
+      expiresIn: "1h",
     });
+
+    res.status(200).json({ token: token, Email: loadedUser.email });
+  } catch (err) {
+    next(err);
+  }
 };
